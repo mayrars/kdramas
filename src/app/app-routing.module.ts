6c2c4047b429ff6eca7027d1215ae6b477e34650
Mayra <mayra203@gmail.com>
2018-06-19 17:57:27 -0500

    Modified routers and added packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
+import { PrincipalComponent } from './public/principal/principal.component';
+import { NotfoundComponent } from './common/notfound/notfound.component';
+import { CommonModule } from '@angular/common';
+import { PublicGuard } from './common/guards/public.guard';
+import { AuthGuard } from './common/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: '/principal'
+	},
+	{
+		path: 'login',
+		pathMatch: 'full',
+		loadChildren: './public/login/login.module#LoginModule',
+		canActivate: [PublicGuard]
+	},
+	{
+		path: 'principal',
+		pathMatch: 'full',
+		loadChildren: './public/principal/principal.module#PrincipalModule',
+		canActivate: [PublicGuard]
+	},
+	{
+		path: 'home',
+		pathMatch: 'full',
+		loadChildren: './auth/home/home.module#HomeModule',
+		canActivate: [AuthGuard]
+	},
+	{ path: '**', pathMatch: 'full', component: NotfoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [
+		CommonModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
+import { AuthGuard } from './common/guards/auth.guard';
+import { AuthenticationService } from './common/services/authentication.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/layout/header/header.component';
+import { AngularFireModule }  from 'angularfire2';
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { PublicGuard } from './common/guards/public.guard';
+import { environment } from '../environments/environment';
+import { NotfoundComponent } from './common/notfound/notfound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		NotfoundComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatIconModule,
+		Ng2Webstorage,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireDatabaseModule,
+		AngularFireAuthModule
+	],
+	providers: [
+		AuthenticationService, //Services
+		AuthGuard, PublicGuard //Guards
+
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
