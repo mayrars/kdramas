9d10d52a4c8f722364d9badb353094c6b26755ed
Mayra <mayra203@gmail.com>
2018-08-23 10:02:35 -0500

    Added routs

--- src/app/auth/home/dramas/dramas-routing.module.ts
+++ src/app/auth/home/dramas/dramas-routing.module.ts
@@ -1,9 +1,21 @@
+import { DramasDetailComponent } from './dramas-detail/dramas-detail.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DramasListComponent } from './dramas-list/dramas-list.component';
+import { NewDramasComponent } from './dramas-list/new-dramas/new-dramas.component';
 
 const routes: Routes = [
   { path: '', component: DramasListComponent },
+  {
+    path: 'detail/:id',
+    pathMatch: 'full',
+    component: DramasDetailComponent
+  },
+  {
+    path: 'new',
+    pathMatch:'full',
+    component: NewDramasComponent
+  }
 ];
 
 @NgModule({
