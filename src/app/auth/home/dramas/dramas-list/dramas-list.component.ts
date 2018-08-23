36d2b89c56f992b270106e0e70686587683d712f
Mayra <mayra203@gmail.com>
2018-08-23 10:08:33 -0500

    Modified Drama list, added functions to delete and add drama

--- src/app/auth/home/dramas/dramas-list/dramas-list.component.ts
+++ src/app/auth/home/dramas/dramas-list/dramas-list.component.ts
@@ -1,22 +1,29 @@
-import { Drama } from './../models/drama';
-import { Observable } from 'rxjs';
-import { DramasService } from './../../../../common/services/dramas.service';
-import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router"
+import { Drama } from "./../models/drama"
+import { Observable } from "rxjs"
+import { DramasService } from "./../../../../common/services/dramas.service"
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-dramas-list',
-  templateUrl: './dramas-list.component.html',
-  styleUrls: ['./dramas-list.component.scss']
+	selector: "app-dramas-list",
+	templateUrl: "./dramas-list.component.html",
+	styleUrls: ["./dramas-list.component.scss"]
 })
 export class DramasListComponent implements OnInit {
-  dramas
-  constructor(
-    public dramaService : DramasService
-  ) {
-    this.dramaService.getAllDramas().subscribe(data=>{
-      this.dramas = data
-    })
-  }
-  ngOnInit() {
-  }
+	dramas
+	drama: Drama
+	constructor(public dramaService: DramasService, public router: Router) {
+		this.dramaService.getAllDramas().subscribe(data => {
+			this.dramas = data
+		})
+	}
+	ngOnInit() {}
+	deleteDrama(event, key) {
+		if (confirm("Are you shure to delete this item?")) {
+			this.dramaService.deleteDrama(key)
+		}
+	}
+	addDrama() {
+		this.router.navigate["new"]
+	}
 }
