3ae8d32270b396032b4ddec5e4293f00bb83eb9b
Mayra <mayra203@gmail.com>
2018-08-23 10:06:47 -0500

    Added Modules

--- src/app/auth/home/dramas/dramas.module.ts
+++ src/app/auth/home/dramas/dramas.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
 
-import { DramasRoutingModule } from './dramas-routing.module';
-import { DramasListComponent } from './dramas-list/dramas-list.component';
-import { MatCardModule, MatButtonModule, MatIconModule } from "@angular/material";
+import { DramasRoutingModule } from "./dramas-routing.module"
+import { DramasListComponent } from "./dramas-list/dramas-list.component"
+import { DramasDetailComponent } from "./dramas-detail/dramas-detail.component"
+import { NewDramasComponent } from "./dramas-list/new-dramas/new-dramas.component"
+import { AngularFireStorageModule } from "angularfire2/storage"
+
+import { NgxDropfilesModule } from "ngx-dropfiles"
+import {
+	MatCardModule,
+	MatButtonModule,
+	MatIconModule,
+	MatInputModule,
+	MatFormFieldModule,
+	MatDatepickerModule,
+	MatNativeDateModule,
+	MatProgressSpinnerModule,
+	MatProgressBarModule
+} from "@angular/material"
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DramasRoutingModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
-  ],
-  declarations: [DramasListComponent]
+	imports: [
+		CommonModule,
+		DramasRoutingModule,
+		MatCardModule,
+		MatButtonModule,
+		MatIconModule,
+		NgxDropfilesModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatDatepickerModule,
+		MatNativeDateModule,
+		AngularFireStorageModule,
+		MatProgressSpinnerModule,
+		MatProgressBarModule
+	],
+	declarations: [
+		DramasListComponent,
+		DramasDetailComponent,
+		NewDramasComponent
+	]
 })
-export class DramasModule { }
+export class DramasModule {}
