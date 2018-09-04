0fb64f9ea77e43f5d2c2e9ceb200babb44f3c55c
Mayra <mayra203@gmail.com>
2018-09-04 18:08:34 -0500

    Added component actor list

--- src/app/auth/home/actors/actors-list/actors-list.component.ts
+++ src/app/auth/home/actors/actors-list/actors-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ActorService } from "./../../../../common/services/actor.service"
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-actors-list',
-  templateUrl: './actors-list.component.html',
-  styleUrls: ['./actors-list.component.scss']
+	selector: "app-actors-list",
+	templateUrl: "./actors-list.component.html",
+	styleUrls: ["./actors-list.component.scss"]
 })
 export class ActorsListComponent implements OnInit {
+	constructor(public actorService: ActorService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {}
+	deleteActor() {}
 
 }
--- src/app/auth/home/actors/actors.module.ts
+++ src/app/auth/home/actors/actors.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core"
+import { CommonModule } from "@angular/common"
 
-import { ActorsRoutingModule } from './actors-routing.module';
-import { ActorsListComponent } from './actors-list/actors-list.component';
+import { ActorsRoutingModule } from "./actors-routing.module"
+import { ActorsListComponent } from "./actors-list/actors-list.component"
+import { MatIconModule, MatButtonModule } from "@angular/material"
+import { ActorService } from "../../../common/services/actor.service"
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ActorsRoutingModule
-  ],
-  declarations: [ActorsListComponent]
+	imports: [
+		CommonModule,
+		ActorsRoutingModule,
+		MatIconModule,
+		MatButtonModule
+	],
+	declarations: [ActorsListComponent],
+	providers: [ActorService]
 })
-export class ActorsModule { }
+export class ActorsModule {}
